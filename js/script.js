34380ec9ff62032f68d415c0490f9ac013a3512f
Torres Marcos <marcos_torres@live.com.ar>
2021-09-19 23:03:40 -0300

    Clase1_Desafio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let numeroUno = prompt("Ingrese el primer numero a multiplicar");
+let numeroDos = prompt("Ingrese el segundo numero a multiplicar");
+let resultado= prompt("El resultado es: " + (numeroUno * numeroDos));
+
+let nombre = prompt("Ingrese su Nombre");
+console.log("Hola " + nombre)
